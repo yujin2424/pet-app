661aa42fa053f7cd30652779346f9e251ce7445f
yujin <kikoio24@naver.com>
2024-10-15 19:49:22 +0900

    tab 수정

--- src/page/AboutPage.js
+++ src/page/AboutPage.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import MainSlider from '../components/MainSlider';
-import Tab from '../components/Tab';
-import List from '../components/List';
+import MainSlider from '../componenets/MainSlider';
+import Tab from '../componenets/Tab';
+import List from '../componenets/List';
 import TabData from '../data/tab.json';
 
-const AboutPage = () => {
-    const listData=TabData.fillter((list)=>list.category === "dog"||list.category==="cat");
-    const tabLabels=[{label: 'dog', value:'dog'},{label: 'cat', value:'cat'}];
-    const aniList=TabData.filter(list=>list.sort==="dog");
 
-    return (
-        <div>
-            <MainSlider />
-            <Tab tabs={listData} tabData={tabLabels} />
-            <List aniList={aniList} />
-        </div>
-    );
+const AboutPage = () => {
+   const listData=TabData.filter((list)=>list.category === "dog"||list.category==="cat");
+   const tabLabels=[
+      {label: 'dog', value:'dog'},
+      {label: 'cat', value:'cat'}, 
+   ];
+   const aniList=TabData.filter(list=>list.sort==="dog")
+   
+   return (
+      <div>
+         <MainSlider />
+         <Tab  tabs={tabLabels} tabData={listData} />
+         <List aniList={aniList} />
+      </div>
+   );
 };
 
 export default AboutPage;

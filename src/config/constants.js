85ea8b7bce006b6a349ef38312d661b07ef1a8b8
yujin <kikoio24@naver.com>
2024-11-01 18:26:07 +0900

    주소변경함

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const API_URL=process.env.NODE_ENV==='production'?
-"https://port-0-pet-server-m2yi1ucbb4337abd.sel4.cloudtype.app"
+"https://port-0-pet-server-m2yj0mm341b6843c.sel4.cloudtype.app"
 : "http://localhost:8080";
